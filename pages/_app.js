fd32c216d1e44adb2d9b926714278e1d148c05b2
Himanshu Singh Chauhan <him.speedo@gmail.com>
2023-01-17 18:13:50 +0530

    firebase connection

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "@material-tailwind/react";
+import { SessionProvider } from "next-auth/react";
+import "../styles.css"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SessionProvider session={pageProps.session}>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
 
