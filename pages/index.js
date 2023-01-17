273a7b58d08b7185677bda41b3399fbc999cc1c3
Himanshu Singh Chauhan <him.speedo@gmail.com>
2023-01-17 18:15:23 +0530

    ultra max pro changes

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,92 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
 import { IconButton } from "@material-tailwind/react";
+import { getSession, useSession } from "next-auth/react";
+import Login from "../components/Login";
+import { useState } from "react";
+
+import { useCollectionOnce } from "react-firebase-hooks/firestore";
+import { orderBy, query } from "firebase/firestore";
+import { onSnapshot } from "firebase/firestore";
+
+import { Fragment } from "react";
+
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+} from "@material-tailwind/react";
+
+import { db } from "../firebase";
+
+import {
+  collection,
+  getDocs,
+  addDoc,
+  doc,
+  serverTimestamp,
+} from "firebase/firestore";
+import DocumentRow from "../components/DocumentRow";
 
 export default function Home() {
+  const { data: session } = useSession();
+  if (!session) return <Login />;
+
+  const [showModal, setShowModal] = useState(false);
+  const [input, setInput] = useState("");
+
+  const colRef = collection(db, "userDocs");
+  // addDoc(colRef, {email: session.user.email})
+
+  const docRef = doc(colRef, session.user.email);
+  const docsRef = collection(docRef, "docs");
+
+  const [ snapshot ] = useCollectionOnce(query(docsRef, orderBy("timestamp")));
+  // console.log(snapshot.docs);
+
+  const createDocument = () => {
+    if (!input) return;
+
+    addDoc(docsRef, { name: input, timestamp: serverTimestamp() });
+
+    // db.collection("userDocs").doc(session.user.email).collection('docs').add({fileName: input, timestamp: firebase.firestore.FieldValue.serverTimestamp()});
+    setInput("");
+    setShowModal(false);
+  };
+
+  const handleOpen = () => setShowModal(!showModal);
+
+  const model = (
+    <Dialog open={showModal} handler={handleOpen}>
+      <DialogHeader>Name of new document</DialogHeader>
+      <DialogBody divider>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          className="outline-none w-full"
+          placeholder="Enter name of document..."
+          onKeyDown={(e) => e.key === "Enter" && createDocument()}
+        />
+      </DialogBody>
+      <DialogFooter>
+        <Button
+          variant="text"
+          color="red"
+          onClick={handleOpen}
+          className="mr-1"
+        >
+          <span>Cancel</span>
+        </Button>
+        <Button variant="gradient" color="green" onClick={createDocument}>
+          <span>Confirm</span>
+        </Button>
+      </DialogFooter>
+    </Dialog>
+  );
+
   return (
     // <div className={styles.container}>
     <div>
@@ -17,6 +101,7 @@ export default function Home() {
       </Head>
 
       <Header></Header>
+      {model}
 
       <section className="bg-[#F8F9FA] pb-10 px-10">
         <div className="max-w-3xl mx-auto">
@@ -30,8 +115,10 @@ export default function Home() {
           <div>
             <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700">
               <Image
-                src="/../public/resources/docs-blank-googlecolors-newdoc.png"
+                src="/resources/docs-blank-googlecolors-newdoc.png"
                 layout="fill"
+                onClick={handleOpen}
+                alt="create new document"
               />
             </div>
             <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">
@@ -47,11 +134,25 @@ export default function Home() {
             <h2 className="font-medium flex-grow">My Documents</h2>
             <p className="mr-12">Date Created</p>
             <IconButton color="gray" variant="text">
-            <i className="material-icons">folder</i>
+              <i className="material-icons">folder</i>
             </IconButton>
           </div>
+
+        {snapshot && snapshot.docs.map((doc) => (
+          <DocumentRow key={doc.id} id = {doc.id} fileName={doc.data().name} date = {doc.data().timestamp} />
+        ))}
         </div>
       </section>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
